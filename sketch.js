35b7353fba9a26c9988655f3a22778ee9e9a70e8
Toust <latostadorano@gmail.com>
2021-10-28 18:43:09 -0500

    Prints all the photos within a photo post

--- sketch.js
+++ sketch.js
@@ -7,15 +7,12 @@ function setup() {
 }
 
 function gotData(tumblr) {
+  // Loop through every post and filter by type (photo)
   for (var i = 0; i < tumblr.response.posts.length; i++) {
-    print(tumblr.response.posts[i].type)
-
-    if (tumblr.response.posts[i].type == 'text') {
-      print("It's a text post");
-
-    } else if (tumblr.response.posts[i].type == 'photo') {
-      //print ("It's a photo post");
-      createImg(tumblr.response.posts[i].photos[0].original_size.url);
+    if (tumblr.response.posts[i].type == 'photo') {
+      for (var j = 0; j < tumblr.response.posts[i].photos.length; j++){
+        createImg(tumblr.response.posts[i].photos[j].original_size.url);
+      }
     }
   }
 }
