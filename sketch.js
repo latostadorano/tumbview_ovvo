f89bf300fe08557e28d75a7fe41dca3d780e446b
Toust <latostadorano@gmail.com>
2021-10-28 18:30:49 -0500

    Only prints image if the type input is = text

--- sketch.js
+++ sketch.js
@@ -8,12 +8,14 @@ function setup() {
 
 function gotData(tumblr) {
   for (var i = 0; i < tumblr.response.posts.length; i++) {
-    // print(tumblr.response.posts[i].type)
-    if (tumblr.response.posts.type == 'text') {
+    print(tumblr.response.posts[i].type)
+
+    if (tumblr.response.posts[i].type == 'text') {
       print("It's a text post");
-      //createImg(tumblr.response.posts[i].photos[0].original_size.url);
-    } else if (tumblr.response.posts.type == 'photo') {
-      print ("It's a photo post");
+
+    } else if (tumblr.response.posts[i].type == 'photo') {
+      //print ("It's a photo post");
+      createImg(tumblr.response.posts[i].photos[0].original_size.url);
     }
   }
 }
